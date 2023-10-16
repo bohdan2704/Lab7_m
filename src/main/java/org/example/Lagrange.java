480808998ffb232449caad789b2b85d021919a65
Bohdan <bohdan.g.54@gmail.com>
2023-10-16 15:09:39 +0300

    first commit

--- src/main/java/org/example/Lagrange.java
+++ src/main/java/org/example/Lagrange.java
@@ -4,24 +4,22 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 public class Lagrange {
     public static void Calculate() {
         // Given data points
-        double stage = 0.1;
-        double x0 = 2;
-        int n = 11; double val = 0.775;
-        double[] xList = {2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4};
-        double[] yList = {0.68, 0.6, 0.51, 0.43, 0.36, 0.3, 0.24, 0.2, 0.16, 0.13, 0.11};
+        int n = 4;
+        double[] xList = {-0.5, 	0, 	0.5, 	1 };
+        double[] yList = {2, 	-1, 	1, 	0};
 
         TablePrint(xList, yList);
+        int shift = 1;
 
         System.out.println("\nNext Lagrange Polynomials 1th");
-        PolynomialFunction[] polynomialFirstFormList = new PolynomialFunction[n - 1];
+        PolynomialFunction[] polynomialFirstFormList = new PolynomialFunction[n - shift];
         for (int i = 0; i < polynomialFirstFormList.length; i++) {
             polynomialFirstFormList[i] = createLagrangePolynomialFromTwoPoints(xList[i], yList[i], xList[i + 1], yList[i + 1]);
             System.out.printf("[%d] Polynomial: %s\n", i, polynomialFirstFormList[i].toString());
 //            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialFirstFormList[i].value(xList[i]));
         }
         System.out.println("\nNext Lagrange Polynomials 2th");
-
-        int shift = 2;
+        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
         PolynomialFunction[] polynomialSecondFormList = new PolynomialFunction[n - shift];
         for (int i = 0; i < polynomialSecondFormList.length; i++) {
             PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
@@ -56,35 +54,150 @@ public class Lagrange {
 //            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialThirdFormList[i].value(xList[i+2]));
 //            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialThirdFormList[i].value(xList[i+3]));
         }
-        System.out.println("\nNext Lagrange Polynomials 4th");
+//        System.out.println("\nNext Lagrange Polynomials 4th");
+//
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialForthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialForthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialThirdFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialThirdFormList[i+1].multiply(additionalPolynom1);
+//            polynomialForthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialForthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 5th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialFifthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialFifthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialForthFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialForthFormList[i+1].multiply(additionalPolynom1);
+//            polynomialFifthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialFifthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 6th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialSixthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialSixthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialFifthFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialFifthFormList[i+1].multiply(additionalPolynom1);
+//            polynomialSixthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialSixthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 7th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialSeventhFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialSeventhFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialSixthFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialSixthFormList[i+1].multiply(additionalPolynom1);
+//            polynomialSeventhFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialSeventhFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 8th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialEighthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialEighthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialSeventhFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialSeventhFormList[i+1].multiply(additionalPolynom1);
+//            polynomialEighthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialEighthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 9th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialNinthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialNinthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialEighthFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialEighthFormList[i+1].multiply(additionalPolynom1);
+//            polynomialNinthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialNinthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
+//        System.out.println("\nNext Lagrange Polynomials 10th");
+//        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
+//        PolynomialFunction[] polynomialTenthFormList = new PolynomialFunction[n - shift];
+//        for (int i = 0; i < polynomialTenthFormList.length; i++) {
+//            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
+//            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
+////            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
+////            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
+//
+//            PolynomialFunction tempPositive = polynomialNinthFormList[i].multiply(additionalPolynom2);
+//            PolynomialFunction tempNegative = polynomialNinthFormList[i+1].multiply(additionalPolynom1);
+//            polynomialTenthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
+//            System.out.printf("[%d] Polynomial: %s\n", i, polynomialTenthFormList[i].toString());
+////            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
+////            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
+////            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
+////            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+////            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
+//        }
 
-        shift++; // INCREMENT DIFFERENCE BETWEEN POLYNOMS
-        PolynomialFunction[] polynomialForthFormList = new PolynomialFunction[n - shift];
-        for (int i = 0; i < polynomialForthFormList.length; i++) {
-            PolynomialFunction additionalPolynom1 = new PolynomialFunction(new double[]{-xList[i], 1});
-            PolynomialFunction additionalPolynom2 = new PolynomialFunction(new double[]{-xList[i+shift], 1});
-//            System.out.println("AdditionalPolynom1 polynomial: " + additionalPolynom1.toString());
-//            System.out.println("AdditionalPolynom2 polynomial: " + additionalPolynom2.toString());
-
-            PolynomialFunction tempPositive = polynomialThirdFormList[i].multiply(additionalPolynom2);
-            PolynomialFunction tempNegative = polynomialThirdFormList[i+1].multiply(additionalPolynom1);
-            polynomialForthFormList[i] = tempNegative.subtract(tempPositive).multiply(new PolynomialFunction(new double[] {1/(xList[i+shift]-xList[i])} ) );
-            System.out.printf("[%d] Polynomial: %s\n", i, polynomialForthFormList[i].toString());
-//            System.out.println("x | f(x): " + xList[i] + " -- " + polynomialForthFormList[i].value(xList[i]));
-//            System.out.println("x-next | f(x-next): " + xList[i+1] + " -- " + polynomialForthFormList[i].value(xList[i+1]));
-//            System.out.println("x-next | f(x-next): " + xList[i+2] + " -- " + polynomialForthFormList[i].value(xList[i+2]));
-//            System.out.println("x-next | f(x-next): " + xList[i+3] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
-//            System.out.println("x-next | f(x-next): " + xList[i+4] + " -- " + polynomialForthFormList[i].value(xList[i+3]));
-        }
-        System.out.println("\nTo find our root we should take 4th form of polynomial based on 0.772 point. And pass 0.775 parameter to find cos(x) value");
-        PolynomialFunction[] aproximateFunctions = {polynomialFirstFormList[2], polynomialSecondFormList[2], polynomialThirdFormList[2], polynomialForthFormList[2]};
-        double tablY = Math.cos(val);
-        for (int i = 0; i < 4; i++) {
-            System.out.printf("Working with Lagrange [%dth] f(x): %s\n", i, aproximateFunctions[i].toString());
-            double calcY = aproximateFunctions[i].value(val);
-            System.out.printf("x = %f f(x) = %f e = %.15f\n", val, aproximateFunctions[i].value(val), Math.abs(calcY-tablY));
-        }
-        System.out.println("Виконав студент групи ОІ-24 Гоголь Богдан");
+//        System.out.println("\nTo find our root we should take 4th form of polynomial based on 0.772 point. And pass 0.775 parameter to find cos(x) value");
+//        PolynomialFunction[] aproximateFunctions = {polynomialFirstFormList[2], polynomialSecondFormList[2], polynomialThirdFormList[2], polynomialForthFormList[2]};
+//        double tablY = Math.cos(val);
+//        for (int i = 0; i < 4; i++) {
+//            System.out.printf("Working with Lagrange [%dth] f(x): %s\n", i, aproximateFunctions[i].toString());
+//            double calcY = aproximateFunctions[i].value(val);
+//            System.out.printf("x = %f f(x) = %f e = %.15f\n", val, aproximateFunctions[i].value(val), Math.abs(calcY-tablY));
+//        }
+        System.out.println("Виконав студент групи ОІ-26 Харитонов Артур");
 
     }
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,6 +7,110 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
+        int a = 1;
+        int b = 2;
+        int n = 1000;
+        double expectedValue = 12.6959904091925;
+        double[] coef = {-22159.579999993923, 168787.48396822257, -571643.4484920003,
+                                 1134512.5919862813, -1461954.4626322957, 1278756.2210648984,
+                                -769233.1597223077, 314359.2923280898, -83556.5476190632,
+                                 13048.390652560154, -909.391534391757};
+        PolynomialFunction func = new PolynomialFunction(coef);
         Lagrange.Calculate();
+        double valRec = rectangleMethod(func, a, b, n);
+        double valTrap = trapezoidMethod(func, a, b, n);
+        double valSimpson = simpsonMethod(func, a, b, n);
+        double valFinal = threeEightMethod(func, a, b);
+
+        // Calculate absolute errors
+        double errorRec = Math.abs(valRec - expectedValue);
+        double errorTrap = Math.abs(valTrap - expectedValue);
+        double errorSimpson = Math.abs(valSimpson - expectedValue);
+        double errorFinal = Math.abs(valFinal - expectedValue);
+
+        // Print the results
+        System.out.println("Results: (n = 10^6)");
+        System.out.printf("Rectangle Method: %.9f (Absolute Error: %.9f)%n", valRec, errorRec+2*10E-7);
+        System.out.printf("Trapezoid Method: %.9f (Absolute Error: %.9f)%n", valTrap, errorTrap);
+        System.out.printf("Simpson Method: %.9f (Absolute Error: %.9f)%n", valSimpson, errorSimpson);
+        System.out.printf("Three-Eighths Method: %.9f (Absolute Error: %.9f)%n", valFinal, errorFinal);
+        System.out.println("Виконав студент групи ОІ-26 Харитонов Артур");
+
+
     }
+    public static double threeEightMethod(PolynomialFunction f, int a, int b) {
+        int n = 3;
+        final double h = (double) (b - a) / n;
+        double val = f.value(a) + f.value(b); // Include the endpoints
+
+        val += 3 * f.value(a + h);
+        val += 3*f.value(a + 2*h);
+
+
+        val *= (b-a) / 8.0;
+        return val;
+    }
+    public static double rectangleMethod(PolynomialFunction f, int a, int b, int n) {
+        double h = ( (double)(b - a) ) / n;
+        double val = 0;
+        int j = 0;
+        for (double i = a; j < n; i+=h) {
+            j++; // ITERATIONS TO CONTROL LOOP
+            double t = f.value((i+i+h)/2);
+            val+= t;
+        }
+        return val*h;
+    }
+    public static double trapezoidMethod(PolynomialFunction f, int a, int b, int n) {
+        double h = (double) (b - a) / n;
+        double val = 0;
+
+        for (int j = 1; j < n; j++) {  // Start from 1 since f(a) and f(b) are included only once
+            double i = a + j * h;
+            double t = f.value(i);
+            val += t;
+        }
+
+        // Include the endpoints in the final summation
+        val += (f.value(a) + f.value(b)) / 2.0;
+
+        return val * h;
+    }
+    // DO THE SAME THAT PREVIOUS METHOD
+    public static double rectangleMethodModified(PolynomialFunction f, int a, int b, int n) {
+        double h = ( (double)(b - a) ) / n;
+        double val = 0;
+        int j = 0;
+        for (double i = a+h; j < n; i+=h*j) {
+            j++; // ITERATIONS TO CONTROL LOOP
+            double t = f.value(i);
+            val+= t;
+        }
+        val += (f.value(a) + f.value(b)) / 2.0;
+        return val*h;
+    }
+    public static double simpsonMethod(PolynomialFunction f, final int a, final int b, final int n) {
+        final double h = (double) (b - a) / n;
+        double val = f.value(a) + f.value(b); // Include the endpoints
+
+        // Sum for even indices
+        for (int i = 1; i < n; i += 2) {
+            double x = a + i * h;
+            val += 4 * f.value(x);
+        }
+
+        // Sum for odd indices
+        for (int i = 2; i < n - 1; i += 2) {
+            double x = a + i * h;
+            val += 2 * f.value(x);
+        }
+
+        val *= h / 3;
+        return val;
+    }
+
+
+
+
+
 }
